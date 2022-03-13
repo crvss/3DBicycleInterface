8b64f7499e513f69961684e3d09140096e6aef9e
Tom <cresswellthomas@yahoo.co.uk>
2022-03-13 22:21:49 +0000

    Attempt at bike Movement and Fixed direction

---
+++ Assets/Scripts/BikeMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BikeMovement : MonoBehaviour
+{
+    [SerializeField] float horizontal = Input.GetAxis("Horizontal");
+    [SerializeField] float vertical = Input.GetAxis("Vertical");
+    float speed = 5.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime;
+    }
+}
